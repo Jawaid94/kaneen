89687f4d1d8dd730591683cd4da95bbf0c79a273
jawaid94 <jawaid94@outlook.com>
2024-01-10 16:02:58 +0300

    [ADD] new module Odoo Shift Key Selection

---
+++ odoo_shift_key_selection/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "Odoo Shift Key Selection",
+
+    'summary': """
+        Shift Selection""",
+
+    'description': """
+        """,
+
+    'author': "Jawaid Iqbal",
+    'website': "https://www.linkedin.com/in/muhammadjawaidiqbal/",
+
+    'category': 'Website',
+    'version': '15.0.1.1',
+
+    'depends': ['web'],
+
+    'assets': {
+        'web.assets_backend': [
+            'odoo_shift_key_selection/static/src/js/form_common.js',
+        ],
+    },
+}
