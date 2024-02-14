32d4349fb5523c85d52b867e00a88d0c85880fea
jawaid94 <jawaid94@outlook.com>
2024-02-14 16:18:21 +0300

    add internal code

--- odoo_magento2_ept/models/stock_move.py
+++ odoo_magento2_ept/models/stock_move.py
@@ -65,7 +65,7 @@ class StockMoveLine(models.Model):
             ]
         }
         for ml in self:
-            if ml.picking_code == 'incoming' and eval(ml.product_id.sku_shatha) is None:
+            if ml.picking_code in ('incoming', 'internal') and eval(ml.product_id.sku_shatha) is None:
                 available_qty = self.env['stock.quant']._get_available_quantity(ml.product_id, ml.location_dest_id,
                                                                                 ml.lot_id, strict=True)
                 payload['skuData'].append(
